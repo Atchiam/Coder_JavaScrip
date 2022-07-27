ed07b09a8a12470e4bd4a7970eb270cf934f087c
Atchiam <braian.ferreyra@hotmail.com>
2022-07-27 16:55:17 -0300

    bucle

---
+++ clase 3 reto/main.js
@@ -0,0 +1,10 @@
+
+
+let e = prompt("cuantos alumnos tenes?")
+
+for (let i=1; i<=e; i++){
+    let a = prompt ("ingresa el nombre de tu alumno");
+
+    alert("tu alumno " + a + " es el " + i +"° de tus alumnos");
+
+}
