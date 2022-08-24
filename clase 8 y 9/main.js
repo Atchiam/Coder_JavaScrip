5bd361537bf02c5550f17d0906bf4387e6311425
Atchiam <braian.ferreyra@hotmail.com>
2022-08-24 12:08:53 -0300

    ya anda gracias santi

--- clase 8 y 9/main.js
+++ clase 8 y 9/main.js
@@ -9,38 +9,46 @@ class User {
     }
 }
 
-const personas = []
+const personas=[]
 let usuario= new User("milanga","cakero","kakyro@hotmai.com",1122333,"milanesa")
 personas.push (usuario)
 
-function datos(){
+function datos(e){
+    e.preventDefault();
+
     let nombre   = document.getElementById("nombre").value
     let apellido = document.getElementById("apellido").value
     let email    = document.getElementById("email").value
     let tel      = Number(document.getElementById("tel").value)
     let comida   = document.getElementById("comida").value
 
-    alert (nombre + apellido + email + tel + comida)
+    alert ("se guardo con exito")
 
     let per= new User(nombre, apellido, email, tel, comida)
     
     personas.push(per);
-
 }
 
 function ver(){
 
-    let p = document.createElement('p')
+    let div = document.createElement('div')
 
-    p.innerHTML = personas.map(e=> "usuario " + e.nombre +" "+ e.apellido)
+    for (let i = 0; i < personas.length; i++) {
+        const e = personas[i];
+        
+        let p = document.createElement('p')
+        p.innerHTML = "usuario " + e.nombre +" "+ e.apellido +"e-mail" +e.email +" "+ "telefono" + e.tel + " comida a eleccion "+ e.comida
     
-    document.body.append(p)
+        div.append(p)
+    }
+    document.body.append(div)
 }
 
-console.log(personas.length)
+console.log(personas)
 
-btn= document.getElementById("enviar")
-btn.onclick = () => {datos()}
+let btn= document.getElementById("enviar")
+console.log(btn)
+btn.addEventListener('click',datos)
 
 btn2= document.getElementById("mostrar")
 btn2.onclick = () => {ver()}
